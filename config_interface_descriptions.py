8e3ece0060a378957ff4f832cfc19213c0a6fe17
Hank Preston <hank.preston@gmail.com>
2021-06-07 00:55:35 +0000

    Argparse script arguments and apply conditional

--- config_interface_descriptions.py
+++ config_interface_descriptions.py
@@ -14,6 +14,20 @@ Goal:
 # Script entry point
 if __name__ == "__main__": 
     print("Deploying standard interface descriptions to network.")
+    # Use argparse retrieve script options : https://docs.python.org/3/library/argparse.html
+    import argparse 
+    parser = argparse.ArgumentParser(description='Deploying standard interface descriptions to network.')
+    parser.add_argument('--testbed', required=True, type=str, help='pyATS Testbed File')
+    parser.add_argument('--sot', required=True, type=str, help='Interface Connection Source of Truth Spreadsheet')
+    parser.add_argument('--apply', action='store_true', help="Should configurations be applied to network. If not set, config not applied.")
+    args = parser.parse_args()
+
+    print(f"Generating interface descriptions from file {args.sot} for testbed {args.testbed}.")
+    if args.apply: 
+        print("Configurations will be applied to network devices.")
+    else: 
+        print("Configurations will NOT be applied to network devices. They will be output to the screen only.")
+
 
     # Read data from CSV source of truth
 
